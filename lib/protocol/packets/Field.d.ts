094b2cf4c8df30deaad97a90d4f1f0ab5511935c
Steven Miller <stevenmiller888@gmail.com>
2019-11-25 10:04:09 -0500

    fix

--- lib/protocol/packets/Field.d.ts
+++ lib/protocol/packets/Field.d.ts
@@ -3,14 +3,14 @@ declare interface Field {
     constructor: {
         name: 'Field'
     };
-    db: string,
-    table: string,
-    name: string,
-    type: string,
-    length: number,
-    string: Function,
-    buffer: Function
-    geometry: Function
+    db: string;
+    table: string;
+    name: string;
+    type: string;
+    length: number;
+    string: Function;
+    buffer: Function;
+    geometry: Function;
 }
 
 export = Field;
