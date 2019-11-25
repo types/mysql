a63e8fbd03aa08c7d66183d70885e6e079b76578
Steven Miller <stevenmiller888@gmail.com>
2019-11-25 16:07:02 -0500

    add field interface (#14)
    * fieldpacket: add .buffer() and .string() to interface
    
    * fieldpacket: add .geometry to interface
    
    * add Field interface
    
    * fix

--- lib/protocol/packets/index.d.ts
+++ lib/protocol/packets/index.d.ts
@@ -2,9 +2,11 @@
 import OkPacket = require('./OkPacket');
 import RowDataPacket = require('./RowDataPacket');
 import FieldPacket = require('./FieldPacket');
+import Field = require('./Field');
 
 export {
     OkPacket,
     RowDataPacket,
-    FieldPacket
+    FieldPacket,
+    Field
 };
